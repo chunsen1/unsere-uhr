6fa1e263f9d04cfd336f27877edb3964aab6400d
Paul Rupek <7033911+pawelrupek@users.noreply.github.com>
2018-01-14 15:02:24 +0100

    IPC entfernt, http-Server hinzugefügt

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
 const later =  require('later'),
       ws281x = require('rpi-ws281x-native'),
       clock = require('./utils/clock'),
-      ipc = require('node-ipc')
+      ipc = require('node-ipc'),
+      express = require('express'),
+      yargs = require('yargs'),
+      bodyParser = require('body-parser'),
+      settings = require('./utils/settings')
 
-var schedule = later.parse.text('every 1 seconds')
-
+// schedule the clock
+let schedule = later.parse.text('every 1 seconds')
 later.setInterval(clock, schedule)
 
+// reset LEDs on exit
 process.on('SIGINT', () => {
     ws281x.reset()
     process.nextTick(() => process.exit(0))
 })
 
-ipc.config.id = 'unsere-uhr-proc';
-ipc.config.retry = 1500;
-ipc.config.silent = true;
+// initialize express
+const app = express()
 
-ipc.serve(() => ipc.server.on('set-config', message => console.log(message)))
+// command line arguments
+const argv = yargs
+    .option('port', {
+        alias: 'p',
+        describe: 'Der Port für den http-Server',
+        default: 8080,
+        type: 'number'
+    })
+    .help()
+    .example('$0 -p 1337')
+    .completion()
+    .epilogue('Mehr informationen gibt es auf https://github.com/chunsen1/unsere-uhr')
+    .argv
 
-ipc.server.start()
+// middleware
+app.use(bodyParser.json())
+
+// routes
+app.get('/settings/light/', (req, res) => res.status(200).json({ 
+    timeSpan: settings.light.getTimeSpan(),
+    minimum: settings.light.getMinimum(),
+    maximum: settings.light.getMaximum(),
+    readInterval: settings.light.getReadInterval()
+}))
+
+app.get('/settings/brightness/', (req, res) => res.status(200).json({
+    strategies: settings.brightness.STRATEGIES,
+    strategy: settings.brightness.getStrategy(),
+    fixedValue: settings.brightness.getFixedValue()
+}))
+
+// start server
+app.listen(argv.port, () => console.log(`\r\n The server is running on localhost:${argv.port}`))
